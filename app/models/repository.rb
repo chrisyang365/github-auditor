b5151b51cf0fd540c2ba91b56045769896967ca4
Christopher Yang <chrisyang365@gmail.com>
2021-11-16 22:17:36 -0800

    added validations

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,3 +1,4 @@
 class Repository < ApplicationRecord
+  validates :name, presence: true
   belongs_to :organization
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+  validates :login, presence: true
+  validates :avatar_url, presence: true
   belongs_to :organization
 end
