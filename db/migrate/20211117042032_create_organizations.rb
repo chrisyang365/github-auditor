30562723bca2ba289d5a58f4919f77e211a5f88b
Christopher Yang <chrisyang365@gmail.com>
2021-11-16 20:21:46 -0800

    created orgs model

---
+++ db/migrate/20211117042032_create_organizations.rb
@@ -0,0 +1,10 @@
+class CreateOrganizations < ActiveRecord::Migration[6.1]
+  def change
+    create_table :organizations do |t|
+      t.string :name
+      t.boolean :two_factor_requirement_enabled
+
+      t.timestamps
+    end
+  end
+end
