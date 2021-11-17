61b5aa40c3294395c7fd196d70480025be4db097
Christopher Yang <chrisyang365@gmail.com>
2021-11-16 21:46:04 -0800

    created repo model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_17_051219) do
+ActiveRecord::Schema.define(version: 2021_11_17_053406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 2021_11_17_051219) do
     t.boolean "two_factor_requirement_enabled"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "repositories", force: :cascade do |t|
+    t.string "name"
+    t.bigint "organization_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["organization_id"], name: "index_repositories_on_organization_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,5 +40,6 @@ ActiveRecord::Schema.define(version: 2021_11_17_051219) do
     t.index ["organization_id"], name: "index_users_on_organization_id"
   end
 
+  add_foreign_key "repositories", "organizations"
   add_foreign_key "users", "organizations"
 end
