eefcbf57ab6624bc74b2d0b7cf1fd3750cde111d
Christopher Yang <chrisyang365@gmail.com>
2021-11-16 22:11:01 -0800

    added avatar url for orgs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_17_053406) do
+ActiveRecord::Schema.define(version: 2021_11_17_055957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_11_17_053406) do
     t.boolean "two_factor_requirement_enabled"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "avatar_url"
   end
 
   create_table "repositories", force: :cascade do |t|
