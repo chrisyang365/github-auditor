a0c37fbfab0f117be4ce81d1e8e520a09221862b
Thomas Zhang <54563161+to-mmy@users.noreply.github.com>
2021-11-25 15:01:25 -0800

    tz - fix User model to have login column, fix typo in User and Organization models

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,7 +1,7 @@
 class Organization < ApplicationRecord
-  validates: :name, presence: true
-  validates: :avatar_url, presence: true
-  validates: :two_factor_requirement_enabled, presence: true
+  validates :name, presence: true
+  validates :avatar_url, presence: true
+  validates :two_factor_requirement_enabled, presence: true
 
   has_many :repositories
   has_many :user_org_tables
