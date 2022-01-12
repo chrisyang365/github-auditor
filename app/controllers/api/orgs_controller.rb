31eaf47c6ba84942104978a4245299276ccefab3
Thomas Zhang <54563161+to-mmy@users.noreply.github.com>
2022-01-11 17:08:17 -0800

    tz - update orgs API to get access token from HTTP Authorization header instead of URL params
    following this tutorial: https://www.pluralsight.com/blog/tutorials/token-based-authentication-rails
    
    header has formats as enumerated here: https://stackoverflow.com/a/44914747
    e.g. Authorization: Token aCcEsS tOkEn

--- app/controllers/api/orgs_controller.rb
+++ app/controllers/api/orgs_controller.rb
@@ -1,12 +1,14 @@
 class Api::OrgsController < ApplicationController
+  before_action :authenticate
+  
   def get
-    user_with_token = User.where(access_token: params[:access_token])
-    if user_with_token.blank?
-      render json: { error: "No user with given token" }, status: :not_found
-    else
-      user = user_with_token[0]
-      user_orgs = user.organizations
-      render json: { organizations: user_orgs }, status: :ok
+    render json: { organizations: @user.organizations }, status: :ok
+  end
+
+  private
+  def authenticate
+    authenticate_or_request_with_http_token do |token, options|
+      @user = User.find_by(access_token: token)
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   #api endpoints here
   namespace :api do
     resources 'auth', only: [:create]
-    get 'organizations/:access_token', to: 'orgs#get'
+    get 'organizations', to: 'orgs#get'
   end
   get '*path', to: 'homepage#index'
 end
