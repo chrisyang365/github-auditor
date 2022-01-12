19dfe89de07b86bbda310b28b9d5204664fba5ba
Christopher Yang <chrisyang365@gmail.com>
2022-01-12 13:11:07 -0800

    test cdn server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,16 +22,16 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = true
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.asset_host = 'http://assets.example.com'
+  config.asset_host = 'https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
