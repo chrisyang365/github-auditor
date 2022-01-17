7fcaf1ac0b80dea49fb16bfd57e25de7d84e5ced
kylestubbs1234 <kylestubbs@ucsb.edu>
2022-01-17 14:48:35 -0800

    added dependabot alert api and routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   namespace :api do
     resources 'auth', only: [:create]
     get 'orgs', to: 'orgs#get'
+    get 'orgs/:org_id/repos/:repo_id/dependabotalerts', to: 'dependabot_alert#get'
   end
   #Resque server here
   mount Resque::Server.new, at: '/jobs'
