6d4c48a58e639732d9f91e0d6566238a1d2598e4
eddythai <edwardthai@ucsb.edu>
2022-01-24 14:46:21 -0800

    fixed model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_24_044612) do
+ActiveRecord::Schema.define(version: 2022_01_24_224351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2022_01_24_044612) do
     t.bigint "repository_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["repository_id"], name: "index_code_alerts_on_repository_id"
   end
 
   create_table "dependabot_alerts", force: :cascade do |t|
@@ -70,5 +71,6 @@ ActiveRecord::Schema.define(version: 2022_01_24_044612) do
     t.string "login"
   end
 
+  add_foreign_key "code_alerts", "repositories"
   add_foreign_key "dependabot_alerts", "repositories"
 end
