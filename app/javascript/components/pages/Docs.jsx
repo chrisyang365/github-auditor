530ed01542c762927a7fffce013895cc302134bd
Thomas Zhang <54563161+to-mmy@users.noreply.github.com>
2022-02-22 17:37:11 -0800

    tz - update Docs page
    refactor to use semantic-ui template for consistent padding
    re-add documentation text
    fix bug of lottie animation inflating load time

--- app/javascript/components/pages/Docs.jsx
+++ app/javascript/components/pages/Docs.jsx
@@ -4,49 +4,63 @@ import { AuthContext } from "../App";
 import NavBar from "../layout/NavBar";
 import Lottie from 'react-lottie';
 import animationData from '../../lotties/code-security.json';
-import { Header } from 'semantic-ui-react';
+import { Grid, Header, Segment } from 'semantic-ui-react';
 
 export default function Docs(props) {
-    const { state } = useContext(AuthContext);
-    const defaultOptions = {
-      loop: true,
-      autoplay: true,
-      animationData: animationData,
-      rendererSettings: {
-        preserveAspectRatio: "xMidYMid slice"
-      }
-    };
+  const { state } = useContext(AuthContext);
+  const animationString = JSON.stringify(animationData);
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: JSON.parse(animationString),
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+      progressiveLoad: true
+    }
+  };
+  // from Semantic UI example https://github.com/Semantic-Org/Semantic-UI-React/blob/master/docs/src/layouts/HomepageLayout.js
+  const HomepageLayout = () => (
+    <Segment style={{ padding: '1em 0em' }} vertical>
+      <Grid container stackable verticalAlign='middle'>
+        <Grid.Row>
+          <Grid.Column width={8}>
+            <Header as='h3' style={{ fontSize: '2em' }}>
+              Your GitHub Security Dashboard
+            </Header>
+            <p style={{ fontSize: '1.33em' }}>
+              Easily view security vulnerabilities from all of your GitHub organizations at a single glance.
+              No more navigating through each organization's individually on GitHub!
+            </p>
+            <Header as='h3' style={{ fontSize: '2em' }}>
+              How to Audit Your Organizations
+            </Header>
+            <p style={{ fontSize: '1.33em' }}>
+              To audit GitHub organizations, go to the organizations
+              tab displayed at the top of the screen. From this screen,
+              we can see all of the organizations that you are apart of.
+              You can then click on each organization and look into the
+              vulnerabilities such as API key or dependabot alerts.
+            </p>
+          </Grid.Column>
+          <Grid.Column floated='right' width={6}>
+            <Lottie options={defaultOptions} />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Segment>
+  )
 
-    return (
-      <div>
-        {state.isLoggedIn ? (
-            <>
-                <NavBar />
-                <div style={{justifyContent: "center", display: "flex", padding: 20 + 'px'}}>
-                    <Header
-                      as="h1"
-                      style={{
-                        fontSize: '6em',
-                        fontWeight: 'normal',
-                        width: '5em',
-                        marginLeft: '3em',
-                        marginTop: '1em',
-                      }}
-                      content="Security dashboard for GitHub Organizations"/>
-                  <Lottie
-                      style={{
-                        marginLeft: "20em"
-                      }}
-                      options={defaultOptions}
-                      height={600}
-                      width={600}
-                  />
-                </div>
+  return (
+    <div>
+      {state.isLoggedIn ? (
+        <>
+          <NavBar />
+          <HomepageLayout />
 
-            </>
-        ) : (
-            <Redirect to="/login" />
-        )}
-    </div>
-    )
+        </>
+      ) : (
+        <Redirect to="/login" />
+      )}
+  </div>
+  )
 }
